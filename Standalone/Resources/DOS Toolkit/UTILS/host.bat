eebeb5dde37bcba1a1f059baaedf4ab73223592c
Alun Bestor <alun.bestor@gmail.com>
2012-09-01 21:53:45 +0300

    Improved networking scripts to simultaneously connect across serial nullmodem (using port 2131) as well as ipx (using port 2130).

--- Standalone/Resources/DOS Toolkit/UTILS/host.bat
+++ Standalone/Resources/DOS Toolkit/UTILS/host.bat
@@ -2,10 +2,12 @@
 
 rem If no port was specified in the Boxer configuration file, use default
 IF "%IPXPORT%"=="" SET IPXPORT=2130
+IF "%SERIALPORT%"=="" SET SERIALPORT=2131
 
 rem Use the user-specified port
 IF NOT "%1"=="" SET IPXPORT=%1
+IF NOT "%2"=="" SET SERIALPORT=%2
 
 ipx true
 ipxnet startserver %IPXPORT%
-ipxnet status
+serial1 nullmodem port:%SERIALPORT%

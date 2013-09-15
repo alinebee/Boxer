12afe7a085b49f044b1782c63fc00792f90d88e8
Alun Bestor <alun.bestor@gmail.com>
2013-09-15 20:13:33 +0300

    Updated Boxer helpbook to 10.6 format and reindexed standalone and regular helpbooks.

--- reindex_help.py
+++ reindex_help.py
@@ -6,11 +6,13 @@ This script (re)generates the help index for every locale.
 """
 
 import os
+import sys
 import subprocess
 
-RESOURCE_PATH = os.path.join(os.path.dirname(__file__), "Resources")
-HELP_FOLDER_NAME = "BoxerHelp"
-HELP_INDEX_NAME = "BoxerHelp.helpindex"
+PROJECT_PATH = os.path.dirname(__file__);
+HELPBOOK_PATH = os.path.join(PROJECT_PATH, "Resources/Boxer.help")
+STANDALONE_HELPBOOK_PATH = os.path.join(PROJECT_PATH, "Standalone/Resources/Help.help")
+HELP_INDEX_NAME = "search.helpindex"
 
 def locales_in_path(path):
 	locales = []
@@ -20,17 +22,20 @@ def locales_in_path(path):
 		
 	return locales
 
-def reindex_help_for_locale(locale):
+def reindex_helpbook(helpbook_path):
 	"""
-	Reindexes the help folder for the specified locale.
+	Reindexes all locales within the helpbook at the specified path.
 	"""
-	locale_path = os.path.join(RESOURCE_PATH, locale)
-	help_path = os.path.join(RESOURCE_PATH, locale, HELP_FOLDER_NAME)
-	index_path = os.path.join(help_path, HELP_INDEX_NAME)
 	
-	subprocess.call(["hiutil", "-C", "-a", "-s", "en", "-m", "2", "-f", index_path, help_path])
+	helpbook_resource_path = os.path.join(helpbook_path, "Contents", "Resources")
+	
+	for locale in locales_in_path(helpbook_resource_path):
+		print u" -- Reindexing locale %s..." % locale
+		locale_path = os.path.join(helpbook_resource_path, locale)
+		index_path = os.path.join(locale_path, HELP_INDEX_NAME)
+		subprocess.call(["hiutil", "-C", "-a", "-s", "en", "-vv", "-m", "2", "-f", index_path, locale_path])
 	# Touch the path so that XCode will see that it has changed when building.
-	os.utime(help_path, None)
+	os.utime(helpbook_path, None)
 
 def kill_helpd():
 	"""
@@ -39,9 +44,14 @@ def kill_helpd():
 	subprocess.call(["killall", "helpd"])
 			
 if __name__ == "__main__":
-	for locale in locales_in_path(RESOURCE_PATH):
-		print u"Reindexing locale %s..." % locale
-		reindex_help_for_locale(locale)
+	if len(sys.argv) > 1:
+		helpbook_paths = sys.argv[1:]
+	else:
+		helpbook_paths = [HELPBOOK_PATH, STANDALONE_HELPBOOK_PATH]
+
+	for helpbook_path in helpbook_paths:
+		print u"Reindexing helpbook %s..." % helpbook_path
+		reindex_helpbook(helpbook_path)
 		
 	print u"Restarting helpd process..."
 	kill_helpd()
